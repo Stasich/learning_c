54bd8f2b54e63cd8ae123cf5f975143c69670bfe
Stasich1 <sromanenko91@gmail.com>
2016-11-02 19:47:23 +0300

    разность двух чисел

---
+++ raznost.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int a,b;
+	cout<<"Введите первое число"<<endl;
+	cin>>a;
+	cout<<"Введите второе число"<<endl;
+	cin>>b;
+	int c=a-b;
+	cout<<"разность чисел А-Б = "<<c<<endl;
+	return 0;
+}
