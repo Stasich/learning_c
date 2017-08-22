de40f3ae6f527d865673d1b7f2194c37cb084026
Stas <sromanenko91@gmail.com>
2017-08-22 05:16:18 +0300

    Решение упражнения 4 главы 9

---
+++ Lafore_exercises/904_disk.cpp
@@ -0,0 +1,97 @@
+// 901_book.cpp
+#include <iostream>
+#include <string>
+using namespace std;
+
+class publication
+{
+    private:
+        string name;
+        float price;
+    public:
+        publication(): name( "N/A" ), price ( 0.0 )
+        {}
+        void getdata();
+        void putdata();
+};
+
+class book : private publication
+{
+    private:
+        int pages;
+    public:
+        book(): publication(), pages ( 0 ) {}
+        void putdata();
+        void getdata();
+};
+
+class type : private publication
+{
+    private:
+        float rec_time;
+    public:
+        type() : publication(), rec_time( 0.0 ) {}
+        void getdata();
+        void putdata();
+};
+
+class disk : private publication
+{
+    private:
+        enum Type{ CD, DVD };
+        Type type;
+    public:
+        disk(): type( CD )
+                {}
+        void getdata();
+        void putdata();
+
+};
+
+int main()
+{
+    disk d1;
+    d1.getdata();
+    d1.putdata();
+    return 0;
+}
+
+void publication::getdata() {
+    cout << "Введиет название книги: "; getline ( cin, name );
+    cout << "Введите стоимость: "; cin >> price; cin.ignore( 50, '\n' );
+}
+
+void book::getdata() {
+    publication::getdata();
+    cout << "Введите количетсво страниц: "; cin >> pages; cin.ignore( 50, '\n' );
+}
+
+void type::getdata() {
+    publication::getdata();
+    cout << "Введите время записи: "; cin >> rec_time; cin.ignore( 50, '\n' );
+}
+
+void disk::getdata() {
+    publication::getdata();
+    char ch;
+    cout << "Выберите тип диска CD или DVD. (c/d): ";
+    cin >> ch;
+    type = (( ch == 'c' ) ? CD : DVD);
+}
+void publication::putdata() {
+    cout << name << " " << price;
+}
+void book::putdata() {
+    publication::putdata();
+    cout << " " << pages << endl;
+}
+
+void type::putdata() {
+    publication::putdata();
+    cout << " " << rec_time << endl;
+}
+
+void disk::putdata() {
+    publication::putdata();
+    cout << "\nТип дика: " << (( type == CD ) ? "CD":"DVD") << endl;
+}
