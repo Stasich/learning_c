bea57dc625223456004e3e3c5d51a6610328f122
Stas <sromanenko91@gmail.com>
2017-08-31 16:28:52 +0300

    Решение упражнения 11 главы 12

---
+++ Lafore_exercises/1011_classarr.cpp
@@ -0,0 +1,36 @@
+// 1011_classarr.cpp
+#include <iostream>
+using namespace std;
+
+class arr1
+{
+    private:
+        int* arr [ 10 ];
+    public:
+        arr1() {
+            for ( int j = 0; j < 10; j++ )      //выделяю в памяти место под 10 массивов из 10 int элементов
+            *(arr + j) = new int [10];
+        }
+        int& operator[] ( int n ){
+            int j = n / 10;
+            int i = n%10;
+            return *( *(arr + j) + i );
+        }
+    ~arr1() {
+        for ( int j = 0; j < 10; j++ ){
+            cout << "Освобождаю память.  ";
+            delete [] *( arr + j );}
+        cout << endl;
+    }
+};
+
+int main()
+{
+    arr1 a1;
+    for ( int j = 0; j < 100; j++ )
+        a1[ j ] = j;
+    for ( int j = 0; j < 100; j++ )
+        cout << a1[ j ] << " ";
+    cout << endl;
+    return 0;
+}
